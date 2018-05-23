f20d8662ebe81fb1b06a5cf2e26e2477ce557926
semochkina <36768972+semochkina@users.noreply.github.com>
2018-05-23 10:35:34 +0600

    changes

--- src/test/java/ru/omsk/sveta/example3/SquareFunction2Test.java
+++ src/test/java/ru/omsk/sveta/example3/SquareFunction2Test.java
@@ -4,19 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.omsk.sveta.example2.SquareFunction;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class SquareFunction2Test {
     @Test
-    public void test() {
+    public void test() throws SquareEqException {
         SquareFunction squareFunction = new SquareFunction(2, 7, -4);
         double maxValue = new SquareFunction2(squareFunction).maxValue();
-        Assert.assertEquals(maxValue, 0.5, SquareFunction.delta);
-
+        assertEquals(maxValue, 0.5, SquareFunction.delta);
+    }
+    @Test(expected = SquareEqException.class)
+    public void testNoRoots() throws SquareEqException {
         SquareFunction squareFunction2 = new SquareFunction(3, -1, 7);
-        try {
-            double maxValue2 = new SquareFunction2(squareFunction2).maxValue();
-            Assert.fail("Baga");
-        } catch (RuntimeException ex) {
-           // ex.printStackTrace();
-        }
+        double maxValue2 = new SquareFunction2(squareFunction2).maxValue();
+        fail("Baga");
+    }
+    @Test
+    public void testTwoRoots() throws SquareEqException{
+        SquareFunction squareFunction3 = new SquareFunction(1, 3, -4);
+        double actual = new SquareFunction2(squareFunction3).maxValue();
+        assertEquals(1, actual,  SquareFunction.delta);
     }
 }
